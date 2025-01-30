d0983622a3a6a3ba9095d8b338d9ce81c2a6ffcd
Prince Eze <pchidireeze@gmail.com>
2025-01-30 22:18:44 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app: Express = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// app.use("/api", "ji");
+
+app.get("/test", (_req: Request, res: Response) => {
+  res.json({ message: "Test successful" });
+});
+
+export default app;
